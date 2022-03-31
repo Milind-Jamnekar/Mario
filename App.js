6e973aa484a09dd1db8cd26c3f2a58314fb16992
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-03-31 11:22:53 +0530

    just a bug

--- App.js
+++ App.js
@@ -5,13 +5,20 @@ const c = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+const gravity = 0.5;
 class Player {
   constructor() {
     this.position = {
       x: 100,
       y: 100,
     };
+    //Velocity of blob
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
 
+    //Dimension of blob
     this.width = 50;
     this.height = 50;
   }
@@ -20,7 +27,27 @@ class Player {
     c.fillStyle = "red";
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
+
+  update() {
+    this.draw();
+    //Downward gravity
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
 }
 
 const player = new Player();
 player.draw();
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+}
+
+animate();
